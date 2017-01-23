a9341a71e47615500fb337506de4a23e9bcdaff7
Zach Schulz <fri.zachschulz@gmail.com>
2017-01-23 12:27:48 +0100

    Uppgift 1

---
+++ 23.01.17 to 30.01.17/Assets/Uppgift1.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Uppgift1 : MonoBehaviour {
+    public int ValA = 10;
+    public int ValB = 5;
+    private int ValC = 0;
+    private int ValD = 0;
+	// Use this for initialization
+	void Start () {
+
+        Debug.Log(ValA); 
+
+        Debug.Log(ValB);
+
+        ValC = ValA;
+        ValD = ValB;
+        ValB = ValC;
+        ValA = ValD;
+            
+        Debug.Log(ValA);
+
+        Debug.Log(ValB);
+        }
+
+        
+	    
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
