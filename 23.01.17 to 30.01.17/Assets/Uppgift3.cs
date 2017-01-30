5cdceb81073540fbcd3a7eea0eb8dcff2daaa28c
Zach Schulz <fri.zachschulz@gmail.com>
2017-01-30 11:01:26 +0100

    Uppgift 3

---
+++ 23.01.17 to 30.01.17/Assets/Uppgift3.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Uppgift3 : MonoBehaviour {
+    public int ValA = 10;
+    public int ValB = 15;
+
+    int Method()
+    {
+        return ValA + ValB;
+        
+    }
+    
+
+    
+	// Use this for initialization
+	void Start () {
+        Debug.Log(ValA + ValB);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
